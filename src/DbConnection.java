b3d7cd571020231bb562c52e31e44f290e83dd5a
Hristo Dobrev <hristo.nankov1@gmail.com>
2024-05-03 12:47:19 +0300

    Removed commented line

--- src/DbConnection.java
+++ src/DbConnection.java
@@ -8,7 +8,6 @@ public class DbConnection {
 	static Connection getConnection() {
 		try	{
 			Class.forName("org.h2.Driver");
-			//connection = DriverManager.getConnection("jdbc:h2:tcp://localhost/C:\\Users\\Daniela\\Desktop\\h2db\\db", "sa", "123456");
 			connection = DriverManager.getConnection("jdbc:h2:~./../db/data", "sa", "123456");
 		} catch(ClassNotFoundException e) {
 			System.out.println("Driver could not connect to the DB:");
